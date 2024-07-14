2ae1095a092ff8492e42e4baf6c93265babc7d04
Dinesh babu J <dineshbabu142004@gmail.com>
2024-07-14 17:34:18 +0530

    Updated with application props

--- src/test/java/DigitalProductsApi.java
+++ src/test/java/DigitalProductsApi.java
@@ -1,6 +1,8 @@
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.microsoft.playwright.APIRequest;
 import com.microsoft.playwright.APIRequestContext;
 import com.microsoft.playwright.APIResponse;
@@ -10,27 +12,44 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
+
 public class DigitalProductsApi {
     private Playwright playWright;
     private APIRequestContext request;
+
+    public DigitalProductsApi() throws FileNotFoundException {
+    }
+
     void createPlayWright(){
         playWright=Playwright.create();
     }
     int brandID;
-    String productTypeCode="PHONE_CREDIT";
-    String cartID="40d5edd5-25b7-4ff7-8ddf-dd327916cce5@blibli";
+    String productTypeCode;
+    String cartID;
     String defaultProviderId;
     String defaultProviderName;
     String skuCode;
     String paymentMethod;
     String orderId;
-    String brandName="Indosat";
-    String deviceid="00d57723-464d-455b-ab81-1cd22f4dda31";
-    String INTERNAL_NAME="Umroh Haji Internet 45hr";
+    String brandName;
+    String deviceid;
+    String INTERNAL_NAME;
+    FileReader reader=new FileReader("application.properties");
+    Properties properties= new Properties();
 
-    void createAPIRequestContext(String url){
+    void createAPIRequestContext(String url) throws IOException {
+        properties.load(reader);
+        productTypeCode= properties.getProperty("productTypeCode");
+        brandName=properties.getProperty("brandname");
+        deviceid=properties.getProperty("deviceid");
+        INTERNAL_NAME=properties.getProperty("internalname");
+        cartID=properties.getProperty("cartID");
         Map<String,String> headers=new HashMap<>();
         headers.put("accept","application/json");
         headers.put("Content-Type","application/json");
@@ -38,7 +57,7 @@ public class DigitalProductsApi {
     }
 
     @Test(priority = 1)
-    public void getItemMapping(){
+    public void getItemMapping() throws IOException {
         createPlayWright();
         createAPIRequestContext("http://digital-catalog.qa2-sg.cld/digital-catalog/api/");
         APIResponse response=request
@@ -65,7 +84,7 @@ public class DigitalProductsApi {
     }
 
     @Test(priority = 2)
-    public void GetDefaultProviderId(){
+    public void GetDefaultProviderId() throws IOException {
         createPlayWright();
         createAPIRequestContext("http://digital-catalog.qa2-sg.cld/digital-catalog/api/");
         APIResponse response=request
@@ -89,7 +108,7 @@ public class DigitalProductsApi {
     }
 
     @Test(priority = 3)
-    public void SaveItemMapping(){
+    public void SaveItemMapping() throws IOException {
         createPlayWright();
         createAPIRequestContext("http://digital-catalog.qa2-sg.cld/digital-catalog/api/");
         JSONObject requestBody = new JSONObject();
@@ -122,7 +141,7 @@ public class DigitalProductsApi {
 
 
     @Test(priority = 4)
-    public void deleteExistingCart(){
+    public void deleteExistingCart() throws IOException {
         createPlayWright();
         createAPIRequestContext("http://x-pulsa.qa2-sg.cld/x-pulsa/api/");
         APIResponse response=request.
@@ -138,13 +157,19 @@ public class DigitalProductsApi {
     }
 
     @Test(priority = 5)
-    public void getSkuCode(){
+    public void getSkuCode() throws IOException {
         createPlayWright();
         createAPIRequestContext("http://digital-catalog.qa2-sg.cld/digital-catalog/api/");
 
         APIResponse response=request
-                .get("merchantDigitalSku/getProductListByFilter", RequestOptions.create().setQueryParam("storeId",10001).setQueryParam("requestId","RANDOM").setQueryParam("channelId","pulsa-web").setQueryParam("clientId","pulsa").setQueryParam("username","username")
-                        .setQueryParam("productType",productTypeCode).setQueryParam("brandName",brandName));
+                .get("merchantDigitalSku/getProductListByFilter", RequestOptions.create().
+                        setQueryParam("storeId",10001).
+                        setQueryParam("requestId","RANDOM").
+                        setQueryParam("channelId","pulsa-web").
+                        setQueryParam("clientId","pulsa").
+                        setQueryParam("username","username").
+                        setQueryParam("productType",productTypeCode).
+                        setQueryParam("brandName",brandName));
         JSONObject jsonResponse = new JSONObject(response.text());
         JSONArray contentArray = jsonResponse.getJSONArray("content");
         for (int i = 0; i < contentArray.length(); i++) {
@@ -171,7 +196,7 @@ public class DigitalProductsApi {
     }
 
     @Test(priority = 6)
-    public void doCheckOut(){
+    public void doCheckOut() throws IOException {
         createPlayWright();
         createAPIRequestContext("http://x-pulsa.qa2-sg.cld/x-pulsa/api/");
          APIResponse response= request
@@ -189,12 +214,14 @@ public class DigitalProductsApi {
     }
 
     @Test(priority = 7)
-    public void setPulsaCart(){
+    public void setPulsaCart() throws IOException {
         HashMap<String,Object> reqBody= new HashMap<>();
         reqBody.put("itemSku",skuCode);
         reqBody.put("operatorCode",brandName);
         reqBody.put("providerName",defaultProviderName);
-
+        System.out.println(reqBody.toString());
+        System.out.println(cartID);
+        System.out.println(productTypeCode);
         createPlayWright();
         createAPIRequestContext("http://x-pulsa.qa2-sg.cld/x-pulsa/api/");
         APIResponse response= request
@@ -213,7 +240,7 @@ public class DigitalProductsApi {
     }
 
     @Test(priority =8)
-    public void addToCart(){
+    public void addToCart() throws IOException {
         HashMap<String,Object> reqBody= new HashMap<>();
         reqBody.put("productType",productTypeCode);
         reqBody.put("itemSku",skuCode);
@@ -237,7 +264,7 @@ public class DigitalProductsApi {
     }
 
     @Test(priority = 9)
-    public void changePaymentMethod(){
+    public void changePaymentMethod() throws IOException {
         createPlayWright();
         createAPIRequestContext("http://x-pulsa.qa2-sg.cld/x-pulsa/api/");
         APIResponse response= request
@@ -254,7 +281,7 @@ public class DigitalProductsApi {
     }
 
     @Test(priority = 10)
-    public void payOrder() throws JsonProcessingException {
+    public void payOrder() throws IOException {
         HashMap<String,Object> reqBody= new HashMap<>();
         reqBody.put("pulsaCartId",cartID);
         reqBody.put("extendedData", new HashMap<String, Object>());
@@ -279,7 +306,7 @@ public class DigitalProductsApi {
     }
 
     @Test(priority = 11)
-    public void approveOrder(){
+    public void approveOrder() throws IOException {
         HashMap<String,Object> reqBody= new HashMap<>();
         reqBody.put("orderId",orderId);
         reqBody.put("extData",new HashMap<String, Object>());
@@ -299,7 +326,7 @@ public class DigitalProductsApi {
     }
 
     @Test(priority = 12)
-    public void getOrderVerify(){
+    public void getOrderVerify() throws IOException {
         createPlayWright();
         createAPIRequestContext("http://x-pulsa.qa2-sg.cld/x-pulsa/api/");
         APIResponse response= request
@@ -310,8 +337,17 @@ public class DigitalProductsApi {
                         .setQueryParam("clientId", "pulsa")
                         .setQueryParam("username", "username")
                         .setQueryParam("orderId",orderId));
-
         System.out.println(response.text());
+        String responseBody = response.text();
+        JsonObject jsonResponse = JsonParser.parseString(responseBody).getAsJsonObject();
+        JsonObject pulsaCartItem = jsonResponse
+                .getAsJsonObject("value")
+                .getAsJsonObject("pulsaOrder")
+                .getAsJsonObject("pulsaCartItem");
+        String providerName = pulsaCartItem.get("providerName").getAsString();
+        String description = pulsaCartItem.get("description").getAsString();
+        Assert.assertEquals(providerName,defaultProviderName);
+        Assert.assertEquals(description,INTERNAL_NAME);
         Assert.assertEquals(200,response.status());
     }
 }
